1915ae68174b8e610d7cdbbf05f48ca320dda92f
Dongjie Wu <dwu.jacob@gmail.com>
2023-08-01 20:30:49 +1000

    test_interaction updated

--- tests/test_interaction.py
+++ tests/test_interaction.py
@@ -66,33 +66,21 @@ def test_midjourney_interactions_ping():
 
 
 def test_midjourney_interactions_test():
-    # req = {
-    #     'id': 100003,
-    #     'guild_id': os.environ['GUILD_ID'],
-    #     'session_id': os.environ['SESSION_ID'],
-    #     'channel_id': os.environ['CHANNEL_ID'],
-    #     'type': 2,
-    #     'data': {'id': 1135434235382079508, 'name': 'test', 'type': 1, 'guild_id': os.environ['GUILD_ID']},
-    #     'application_id': os.environ['APPLICATION_ID'],
-    #     'token': "fjdskfjakj",
-    #     'version': os.environ['VERSION_ID']
-    # }
     setting = get_settings()
     req = {"id": 213123123,
            "version": 1,
            "type": 2,
            "token": "hsdjfasdjf",
            "application_id": "1128110065900081334",
-           "guild_id": "1125988642519785584",
-           "channel_id": "1125988643178299425",
+           "guild_id": setting.guild_id,
+           "channel_id": setting.channel_id,
            "session_id": "077cc0c4b93a7090a4e80229f33a8089",
            "data": {"version": "1135434235382079509",
                     "id": "1135434235382079508",
-                    "guild_id": "1125988642519785584",
+                    "guild_id": setting.guild_id,
                     "name": "test",
                     "type": 1,
                     "options": [{"type": 3, "name": "arg", "value": "a"}]}}
-
     interaction = Interaction(**req)
     assert type(interaction) == Interaction
     response = client.post("/midjourney/interactions",
